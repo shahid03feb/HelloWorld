91d4441f9db2af6a8262737b26784ff9bebde691
shahid <shine.shahid@gmail.com>
2018-11-18 20:30:53 -0500

    printing class name and method

---
+++ src/com/zaptle/Helloworld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.zaptle;
+
+/**
+ * @author Shahid
+ *
+ */
+public class Helloworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Helloworld.main()");
+
+	}
+
+}
